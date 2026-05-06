c7f58835bbe70f354532d8ecc7f9c18194665a32
Danny Boy (OpenClaw) <danny@d-expert.com>
2026-05-06 22:17:24 +0000

    refactor: remove eyal-db-instance-1 from RDS module

--- modules/database/rds.tf
+++ modules/database/rds.tf
@@ -50,22 +50,3 @@ resource "aws_db_instance" "dev_db_instance_1" {
   }
 }
 
-resource "aws_db_instance" "eyal_db_instance_1" {
-  identifier           = "eyal-db-instance-1"
-  engine               = "aurora-postgresql"
-  engine_version       = "17.4"
-  instance_class       = "db.serverless"
-  allocated_storage    = 1
-  username             = "admin"
-  password             = "TempPassword123!"
-  skip_final_snapshot  = true
-
-  # Assuming standard VPC security group and subnet groups
-  vpc_security_group_ids = [aws_security_group.db.id]
-  db_subnet_group_name   = aws_db_subnet_group.default.name
-
-  tags = {
-    Name = "eyal-db-instance-1"
-    Environment = "Dev"
-  }
-}
