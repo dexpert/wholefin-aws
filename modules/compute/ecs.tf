6a194fec65d6eef8d85c7497b628860111f34dd8
Danny Boy (OpenClaw) <danny@d-expert.com>
2026-05-06 21:47:24 +0000

    feat: attach kong ecs service to alb target group

--- modules/compute/ecs.tf
+++ modules/compute/ecs.tf
@@ -141,6 +141,12 @@ resource "aws_ecs_service" "kong" {
   desired_count   = 1
   launch_type     = "FARGATE"
 
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = "kong"
+    container_port   = 80
+  }
+
   network_configuration {
     subnets          = var.private_subnets
     security_groups  = [aws_security_group.ecs_tasks.id]
