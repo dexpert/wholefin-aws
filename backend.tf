a449e3661feac1bee9fa59ff8bf07d42478c5b1c
Danny Boy (OpenClaw) <danny@d-expert.com>
2026-05-07 21:42:41 +0000

    chore: remove legacy tf files replaced by Terragrunt

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "wholefin-tfstate-sand-844486820647"
-    key            = "sand/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "wholefin-tfstate-lock"
-    encrypt        = true
-  }
-}
